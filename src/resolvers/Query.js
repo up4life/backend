07becb7442194827390bd486645f73cd977b3ed1
Holden B <holden.bucher.92@gmail.com>
2019-02-19 21:50:57 -0600

    idk

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -10,32 +10,24 @@ const Query = {
 		}
 		return db.query.user(
 			{
-				where: { id: request.userId },
+				where: { id: request.userId }
 			},
-			info,
+			info
 		);
 	},
 	user(parent, args, { db }, info) {
 		// finds a user based on the args provided in the mutation
 		return db.query.user(
 			{
-				...args,
+				...args
 			},
-			info,
+			info
 		);
 	},
 	async getEvents(parent, { location, alt, page, ...args }, ctx, info) {
-		// const categories = args.categories
-		// 	? args.categories
-
-		let cats = args.categories.length
+		let cats = args.categories
 			? args.categories
-			: [
-					'KZFzniwnSyZfZ7v7nJ',
-					'KZFzniwnSyZfZ7v7na',
-					'KZFzniwnSyZfZ7v7nE',
-					'KZFzniwnSyZfZ7v7n1',
-				];
+			: ['KZFzniwnSyZfZ7v7nJ', 'KZFzniwnSyZfZ7v7na', 'KZFzniwnSyZfZ7v7nE', 'KZFzniwnSyZfZ7v7n1'];
 
 		const dates = args.dates ? args.dates.toString() : 'all';
 
@@ -50,12 +42,11 @@ const Query = {
 		if (response.data.page.totalElements > 20) {
 			while (uniques.length < 20) {
 				page = page + 1;
-				//size = 20 - uniques.length;
 				let res = await fetchEvents(location, cats, dates, page, 200);
 
 				if (!res.data) break;
 				else {
-					events = [ ...events, ...res.data._embedded.events ];
+					events = [...events, ...res.data._embedded.events];
 					uniques = res.data._embedded.events.reduce((a, t) => {
 						if (!a.includes(t.name)) a.push(t.name);
 						return a;
@@ -82,16 +73,17 @@ const Query = {
 			total_items: response.data.page.totalElements,
 			page_total: response.data.page.totalPages,
 			page_number: response.data.page.number,
-			location: location,
+			location: location
 		};
 	},
 
 	async getEvent(parent, args, ctx, info) {
 		const { data } = await axios.get(
-			`https://app.ticketmaster.com/discovery/v2/events/${args.id}.json?apikey=${process.env
-				.TKTMSTR_KEY}`,
+			`https://app.ticketmaster.com/discovery/v2/events/${args.id}.json?apikey=${
+				process.env.TKTMSTR_KEY
+			}`
 		);
-		const [ img ] = data.images.filter(img => img.ratio === '4_3');
+		const [img] = data.images.filter(img => img.ratio === '4_3');
 		return {
 			title: data.name,
 			id: data.id,
@@ -100,13 +92,13 @@ const Query = {
 				city: data._embedded ? data._embedded.venues[0].city.name : 'poop',
 				venue: data._embedded ? data._embedded.venues[0].name : 'poopoo',
 				address: data._embedded ? data._embedded.venues[0].address.line1 : 'damnit 3',
-				zipCode: data._embedded ? data._embedded.venues[0].postalCode : 'shit 4',
+				zipCode: data._embedded ? data._embedded.venues[0].postalCode : 'shit 4'
 			},
 			// img in 3_2 or 16_9 ratio is nicer quality, just need to figure out how to get it to be responsive
 			// or we could keep it at 4_3 i'm cool either way
 			image_url: img.url,
 			description: data.info,
-			times: [ data.dates.start.dateTime ],
+			times: [data.dates.start.dateTime]
 			// data.dates.status.code (might be good for things like rescheduled events)
 			// data.pleaseNote (has additional info for things that are rescheduled or something like that it seems)
 			// optional data points to include
@@ -121,8 +113,9 @@ const Query = {
 	},
 	async getLocation(parent, { latitude, longitude }, ctx, info) {
 		const location = await axios.get(
-			`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude}, ${longitude}&key=${process
-				.env.GOOGLE_API_KEY}`,
+			`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude}, ${longitude}&key=${
+				process.env.GOOGLE_API_KEY
+			}`
 		);
 		let city = location.data.results[0].address_components[3].long_name;
 		let state = location.data.results[0].address_components[5].short_name;
@@ -131,16 +124,17 @@ const Query = {
 
 		return {
 			city: `${city}, ${state}`,
-			county: `${county}, ${state}`,
+			county: `${county}, ${state}`
 		};
 	},
 	async locationSearch(parent, args, { db }, info) {
 		const response = await axios(
-			`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${args.city}&types=(cities)&key=${process
-				.env.GOOGLE_API_KEY}`,
+			`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${
+				args.city
+			}&types=(cities)&key=${process.env.GOOGLE_API_KEY}`
 		);
 		const results = response.data.predictions;
-		console.log(results);
+		// console.log(results);
 		const city = results.map(result => {
 			return { city: result.description };
 		});
@@ -148,8 +142,9 @@ const Query = {
 	},
 	async geoHash(parent, { city }, { db }, info) {
 		const response = await axios(
-			`https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${process.env
-				.GOOGLE_API_KEY}`,
+			`https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${
+				process.env.GOOGLE_API_KEY
+			}`
 		);
 		let { lat, lng } = response.data.results[0].geometry.location;
 
@@ -166,11 +161,11 @@ const Query = {
 			{
 				where: {
 					user: {
-						id: args.userId,
-					},
-				},
+						id: args.userId
+					}
+				}
 			},
-			info,
+			info
 		);
 	},
 	async getRemainingDates(parent, args, ctx, info) {
@@ -182,7 +177,7 @@ const Query = {
 			{ where: { id: userId } },
 			`
 				{id permissions events {id}}
-			`,
+			`
 		);
 		// TO DO: define subscription level and benefit!!!
 		let datesCount = 5;
@@ -200,11 +195,11 @@ const Query = {
 			{ where: { id: userId } },
 			`
 				{id permissions events {id}}
-			`,
+			`
 		);
 
 		const invoices = await stripe.invoices.list({
-			customer: user.stripeCustomerId,
+			customer: user.stripeCustomerId
 		});
 		console.log(
 			invoices.map(invoice => ({
@@ -213,13 +208,13 @@ const Query = {
 				amount_paid: invoice.amount_paid,
 				date: invoice.date,
 				hosted_url: invoice.hosted_invoice_url,
-				pdf_url: invoice.invoice_pdf,
-			})),
+				pdf_url: invoice.invoice_pdf
+			}))
 		);
 		return {
-			message: 'invoices',
+			message: 'invoices'
 		};
-	},
+	}
 };
 
 module.exports = Query;
