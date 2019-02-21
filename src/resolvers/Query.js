b85e7adf685aa03f87cee26292dc7fb804dbdd1b
Holden B <holden.bucher.92@gmail.com>
2019-02-20 23:07:47 -0600

    a

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -27,11 +27,11 @@ const Query = {
 	async getEvents(parent, { location, alt, page, ...args }, ctx, info) {
 		location = location.split(',')[0].toLowerCase();
 		console.log(args.categories.length);
-		let cats = args.categories.length
+		let cats = args.categories
 			? args.categories
 			: ['KZFzniwnSyZfZ7v7nJ', 'KZFzniwnSyZfZ7v7na', 'KZFzniwnSyZfZ7v7nE', 'KZFzniwnSyZfZ7v7n1'];
 		console.log(location, cats, args.dates, page);
-		const dates = args.dates.length ? setDates(args.dates.toString()) : undefined;
+		const dates = args.dates ? setDates(args.dates.toString()) : undefined;
 		console.log(dates);
 		let events;
 		let response = await fetchEvents(location, cats, dates, page, 200);
