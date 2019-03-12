fb56a345f129312daf6f775d77cde8926b80caa6
Lauren <llwor94@gmail.com>
2019-03-12 10:48:06 -0700

    cool

--- src/index.js
+++ src/index.js
@@ -32,6 +32,14 @@ server.express.use(async (req, res, next) => {
 	next();
 });
 
-server.start(details => {
-	console.log(`Server is now running on port http://localhost:${details.port}`);
-});
+server.start(
+	{
+		cors: {
+			credentials: true,
+			origin: process.env.FRONTEND_URL,
+		},
+	},
+	details => {
+		console.log(`Server is now running on port http://localhost:${details.port}`);
+	},
+);
