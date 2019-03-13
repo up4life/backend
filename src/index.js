c779ec48a76b73773dc426e3ba7f0e9bb86bfc1c
Holden B <holden.bucher.92@gmail.com>
2019-03-13 13:15:04 -0500

    cors again

--- src/index.js
+++ src/index.js
@@ -33,12 +33,12 @@ server.express.use(async (req, res, next) => {
 });
 
 server.start(
-	// {
-	// 	cors: {
-	// 		credentials: true,
-	// 		origin: process.env.FRONTEND_URL,
-	// 	},
-	// },
+	{
+		cors: {
+			credentials: true,
+			origin: process.env.FRONTEND_URL
+		}
+	},
 	details => {
 		console.log(`Server is now running on port http://localhost:${details.port}`);
 	}
