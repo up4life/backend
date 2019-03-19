9dcd13e2cdb1d402058026825c0ac4c78d8a888a
Lauren <llwor94@gmail.com>
2019-03-18 20:43:03 -0700

    ok

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
-require("dotenv").config({ path: "./.env" });
-const { ApolloServer } = require("apollo-server-express");
-const cookieParser = require("cookie-parser");
-const express = require("express");
-const https = require("https");
-const http = require("http");
-// const fs = require("fs");
+require('dotenv').config({ path: './.env' });
+const { ApolloServer } = require('apollo-server-express');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
 
-const schema = require("./src/schema");
-const { bindings } = require("./src/db");
-const { isAuth, populateUser } = require("./src/middleware/index");
+const schema = require('./src/schema');
+const { bindings } = require('./src/db');
+const { isAuth, populateUser } = require('./src/middleware/index');
 
 const apolloServer = new ApolloServer({
 	schema,
 	context: ({ req }) => ({
 		...req,
-		db: bindings
+		db: bindings,
 	}),
 	playground: true,
 	introspection: true,
-	debug: process.env.NODE_ENV === "development"
+	debug: process.env.NODE_ENV === 'development',
 });
 
-// const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 const corsConfig = {
 	origin: [
-		"https://www.up4.life",
-		"/.herokuapp.com$/",
-		"http://localhost:4000",
-		"https://up4.life",
-		"up4.life"
+		'https://www.up4.life',
+		'/.herokuapp.com$/',
+		'http://localhost:4000',
+		'https://up4.life',
+		// "up4.life"
 	],
-	credentials: true
+	credentials: true,
 };
 const configurations = {
 	production: {
 		ssl: true,
 		port: process.env.PORT || 4000,
-		// hostname: "localhost"
-		hostname: "api.up4.life"
+		hostname: 'localhost',
+		// hostname: "api.up4.life"
 	},
-	development: { ssl: false, port: process.env.PORT || 4000, hostname: "localhost" }
+	development: { ssl: false, port: process.env.PORT || 4000, hostname: 'localhost' },
 };
 
-// const environment = process.env.NODE_ENV || "production";
-const config = configurations["production"];
+const environment = process.env.NODE_ENV || 'production';
+const config = configurations[environment];
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -54,16 +54,16 @@ app.use(express.json());
 app.use(isAuth);
 app.use(populateUser);
 
-apolloServer.applyMiddleware({ app, cors: corsConfig, path: "/" });
+apolloServer.applyMiddleware({ app, cors: corsConfig, path: '/' });
 
 var server;
 if (config.ssl) {
 	server = https.createServer(
 		// {
-		// 	key: fs.readFileSync(`./Certs/www_up4_life.key`),
-		// 	cert: fs.readFileSync(`./Certs/www_up4_life.pem`)
+		//     key: fs.readFileSync(`./Certs/www_up4_life.key`),
+		//     cert: fs.readFileSync(`./Certs/www_up4_life.pem`)
 		// },
-		app
+		app,
 	);
 } else {
 	server = http.createServer(app);
@@ -71,11 +71,9 @@ if (config.ssl) {
 
 apolloServer.installSubscriptionHandlers(server);
 
-server.listen(process.env.PORT, () =>
+server.listen(port, () =>
 	console.log(
-		"🚀 Server ready at",
-		`http${config.ssl ? "s" : ""}://${config.hostname}:${process.env.PORT}${
-			apolloServer.graphqlPath
-		}`
-	)
+		'🚀 Server ready at',
+		`http${config.ssl ? 's' : ''}://${config.hostname}:${port}${apolloServer.graphqlPath}`,
+	),
 );
