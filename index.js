395afd5b181fbfb1e302795bb0630d192a7a75bc
Lauren <llwor94@gmail.com>
2019-03-21 21:38:27 -0700

    ok

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
-require("dotenv").config({ path: "./.env" });
-const { ApolloServer } = require("apollo-server-express");
-const cookieParser = require("cookie-parser");
-const express = require("express");
-const http = require("http");
+require('dotenv').config({ path: './.env' });
+const { ApolloServer } = require('apollo-server-express');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const http = require('http');
 
-const schema = require("./src/schema");
-const { bindings } = require("./src/db");
-const { isAuth, populateUser } = require("./src/middleware/index");
+const schema = require('./src/schema');
+const { bindings } = require('./src/db');
+const { isAuth, populateUser } = require('./src/middleware/index');
 
 const apolloServer = new ApolloServer({
 	schema,
 	context: ({ req }) => ({
 		...req,
-		db: { ...bindings.query, ...bindings.mutation, subscription: bindings.subscription }
+		db: { ...bindings.query, ...bindings.mutation, subscription: bindings.subscription },
 	}),
 	playground: true,
 	introspection: true,
-	debug: process.env.NODE_ENV === "development"
+	debug: process.env.NODE_ENV === 'development',
 });
 
 const corsConfig = {
 	origin: [
-		"http://localhost:3000",
-		"https://www.up4.life",
-		"https://up4lifee.herokuapp.com",
-		"*.herokuapp.com",
-		"http://up4lifee.herokuapp.com"
+		'http://localhost:3000',
+		'https://www.up4.life',
+		'https://up4.life',
+		'https://up4lifee.herokuapp.com',
+		'*.herokuapp.com',
+		'http://up4lifee.herokuapp.com',
 	],
-	credentials: true
+	credentials: true,
 };
 
 const app = express();
 
 app.use(
-	"*",
+	'*',
 	// express.urlencoded({ extended: true }),
 	cookieParser(),
 	express.json(),
 	isAuth,
-	populateUser
+	populateUser,
 );
 
 // app.use("/", isAuth);
@@ -51,12 +52,12 @@ app.use(
 // app.use(isAuth);
 // app.use(populateUser);
 
-apolloServer.applyMiddleware({ app, cors: corsConfig, path: "/" });
+apolloServer.applyMiddleware({ app, cors: corsConfig, path: '/' });
 
 const server = http.createServer(app);
 
 apolloServer.installSubscriptionHandlers(server);
 
 server.listen(process.env.PORT || 4000, () => {
-	console.log("woo server uppp");
+	console.log('woo server uppp');
 });
