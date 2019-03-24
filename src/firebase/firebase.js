cd4646d1be631e2ce06e4c41a43d9191f0a91c09
Lauren <llwor94@gmail.com>
2019-03-24 13:54:15 -0700

    otur

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,18 +1,18 @@
-const firebaseAdmin = require("firebase-admin");
+const firebaseAdmin = require('firebase-admin');
 
 const admin = firebaseAdmin.initializeApp(
 	{
 		credential: firebaseAdmin.credential.cert({
-			type: "service_account",
+			type: 'service_account',
 			project_id: process.env.FIREBASE_PROJECT_ID,
 			private_key_id: process.PRIVATE_KEY_ID,
-			private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+			private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
 			client_email: process.env.FIREBASE_CLIENT_EMAIL,
-			client_id: process.env.FIREBASE_CLIENT_ID
+			client_id: process.env.FIREBASE_CLIENT_ID,
 		}),
-		databaseURL: process.env.FIREBASE_DATABASE_URL
+		databaseURL: process.env.FIREBASE_DATABASE_URL,
 	},
-	"server"
+	'server',
 );
 
 const createUserToken = async (args, ctx) => {
@@ -25,42 +25,39 @@ const createUserToken = async (args, ctx) => {
 	const expiresIn = 60 * 60 * 24 * 5 * 1000;
 
 	if (!(new Date().getTime() / 1000 - decodedIdToken.auth_time < 5 * 60))
-		return { error: { message: "Recent sign in required!" }, token: null };
+		return { error: { message: 'Recent sign in required!' }, token: null };
 
 	const sessionCookie = await admin.auth().createSessionCookie(idToken, { expiresIn });
 
-	ctx.res.cookie("session", sessionCookie, {
+	ctx.res.cookie('session', sessionCookie, {
 		maxAge: 60 * 60 * 24 * 5 * 1000,
 		httpOnly: true,
-		domain: ".up4.life"
+		domain: 'up4.life',
 		// secure: true
 	});
 
 	return sessionCookie
 		? sessionCookie
-		: { error: "User Session Token Creation Error", token: null };
+		: { error: 'User Session Token Creation Error', token: null };
 };
 
 const verifyUserToken = async token => {
-	const claims = await admin
-		.auth()
-		.verifySessionCookie(token, true)
-		.catch(error => {
-			return {
-				error: {
-					message: "User Session Token Verification Error",
-					stack: error
-				},
-				claims: null
-			};
-		});
+	const claims = await admin.auth().verifySessionCookie(token, true).catch(error => {
+		return {
+			error: {
+				message: 'User Session Token Verification Error',
+				stack: error,
+			},
+			claims: null,
+		};
+	});
 
 	return claims
 		? claims
 		: {
-				error: { message: "User Session Token Verification Error" },
-				claims: null
-		  };
+				error: { message: 'User Session Token Verification Error' },
+				claims: null,
+			};
 };
 
 const setUserClaims = (uid, data) => admin.auth().setCustomUserClaims(uid, data);
@@ -81,5 +78,5 @@ module.exports = {
 	setUserClaims,
 	verifyIdToken,
 	getUserRecord,
-	getUID
+	getUID,
 };
